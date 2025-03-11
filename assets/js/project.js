12437c47bd9a306a4dc163ba04447aaf24853b84
shanzefatima <125336030+shanzefatima@users.noreply.github.com>
2025-03-11 19:10:25 -0400

    minor updates

--- assets/js/project.js
+++ assets/js/project.js
@@ -3,55 +3,93 @@ AOS.init();
 /* Project Cards */
 const projectcards = document.querySelector(".projectcards");
 
-// Array of object for projects
+// Array of objects for projects
 const projects = [
-  {
-    title: "Audio Art",
-    cardImage: "assets/images/project-page/p1.jpg",
-    description: "Exploration between visuals and audio",
-    tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    Previewlink: "https://medium.com/@sj3802/audio-art-b79894ae2a25",
-    Githublink: "https://shanzefatima.github.io/CCLAB/Project%20B/main.html"
-  },
+  
+
   {
     title: "A 3D DATA FOREST OF COVID-19'S IMPACT IN SOUTH ASIA",
     cardImage: "assets/images/project-page/02.jpg",
-    description: "In my project, I explored how certain demographic indicators were affected during the course of the pandemic in relation to cases, deaths, and population.",
+    //description: "In my project, I explored how certain demographic indicators were affected during the course of the pandemic in relation to cases, deaths, and population.",
     tagimg: "https://miro.medium.com/max/2800/0*U2DmhXYumRyXH6X1.png",
     Previewlink: "https://medium.com/@sj3802/the-covid-19-data-forest-data-visualization-of-the-pandemics-global-impact-35ca0b206512",
-    Githublink: "https://s18798.pcdn.co/shanghai-shanze/wp-content/uploads/sites/28491/2023/12/DCMI-updated-poster.pdf"
+    //Githublink: "https://s18798.pcdn.co/shanghai-shanze/wp-content/uploads/sites/28491/2023/12/DCMI-updated-poster.pdf",
+
   }
+  , 
+ {
+  title: "Sustainable Energy Management via IoT — Implementation",
+  cardImage: "assets/images/project-page/p3.jpg",
+  //description: "In my project, I explored how certain demographic indicators were affected during the course of the pandemic in relation to cases, deaths, and population.",
+  tagimg: "https://miro.medium.com/max/2800/0*U2DmhXYumRyXH6X1.png",
+  Previewlink: "https://medium.com/@sj3802/sustainable-energy-management-via-iot-implementation-19887962fa39",
+  //Githublink: "https://s18798.pcdn.co/shanghai-shanze/wp-content/uploads/sites/28491/2023/12/DCMI-updated-poster.pdf",
+
+}
+, 
+
+{
+  title: "Digital Product Design: Meal App"  ,
+  cardImage: "assets/images/project-page/p8.jpg",
+  //description: "In my project, I explored how certain demographic indicators were affected during the course of the pandemic in relation to cases, deaths, and population.",
+  tagimg: "https://miro.medium.com/max/2800/0*U2DmhXYumRyXH6X1.png",
+  Previewlink: "https://medium.com/@sj3802/digital-product-design-meal-app-db3d1d6199dd",
+  //Githublink: "https://s18798.pcdn.co/shanghai-shanze/wp-content/uploads/sites/28491/2023/12/DCMI-updated-poster.pdf",
+
+}
+
+, 
+
+{
+  title: "Redesigning Snapchat Interface"  ,
+  cardImage: "assets/images/project-page/p8.jpg",
+  //description: "In my project, I explored how certain demographic indicators were affected during the course of the pandemic in relation to cases, deaths, and population.",
+  tagimg: "https://miro.medium.com/max/2800/0*U2DmhXYumRyXH6X1.png",
+  Previewlink: "https://medium.com/@sj3802/digital-product-design-meal-app-db3d1d6199dd",
+  //Githublink: "https://s18798.pcdn.co/shanghai-shanze/wp-content/uploads/sites/28491/2023/12/DCMI-updated-poster.pdf",
+
+}
 ];
 
+// Function to render project cards
 const showCards = () => {
   let output = "";
-  projects.forEach(
-    ({ title, cardImage, Previewlink, Githublink }) => {
-      output += `       
-        <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
-          <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
-            <div class="header">
-            </div>
-            <div class="data">
-              <div class="content">
-                <div class="title-div">
-                  <h1 class="title"><a href="${Previewlink}" target="_blank">${title}</a></h1> <!-- Add href attribute here -->
-                </div>
-                <ul class="menu-content"><br>
-                  <li><a href="${Previewlink}" class="social-icon">...</a></li>
-                  <li><a href="${Githublink}" class="social-icon">...</a></li>
-                </ul>
+  projects.forEach((project, index) => {
+    output += `       
+      <div class="column skill-card card" style="margin: 15px" data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600">
+        <div class="wrapper" style="background: url(${project.cardImage}) center / cover no-repeat;">
+          <div class="header">
+          </div>
+          <div class="data">
+            <div class="content">
+              <div class="title-div">
+                <h1 class="title"><a href="${project.Previewlink}" target="_blank">${project.title}</a></h1>
               </div>
+              <ul class="menu-content"><br>
+                <li><a href="${project.Previewlink}" class="social-icon">...</a></li>
+                <li><a href="${project.Githublink}" class="social-icon">...</a></li>
+              </ul>
             </div>
           </div>
-        </div>`;
-    }
-  );
+        </div>
+      </div>`;
+  });
   projectcards.innerHTML = output;
+
+  // Add event listeners to buttons
+  document.querySelectorAll('.btn-primary').forEach(button => {
+    button.addEventListener('click', () => {
+      const index = button.getAttribute('data-index');
+      const caseStudyContent = projects[index].caseStudy;
+      document.querySelector('#caseStudyModal .modal-body').innerHTML = caseStudyContent;
+    });
+  });
 };
 
+// Render project cards when the DOM is loaded
 document.addEventListener("DOMContentLoaded", showCards);
 
+// Search functionality
 function myFunction() {
   // Declare variables
   var input, i, skillcard, card, title;
@@ -63,9 +101,7 @@ function myFunction() {
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < title.length; i++) {
-    if (
-      title[i].innerHTML.toUpperCase().includes(input)
-    ) {
+    if (title[i].innerHTML.toUpperCase().includes(input)) {
       skillcard[i].style.display = "";
       card[i].style.display = "";
     } else {
